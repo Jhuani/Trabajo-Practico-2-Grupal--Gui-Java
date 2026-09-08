a83470dae7f52bf562d46b902db78ec336864071
Pcarnota <pablocarnotacai@gmail.com>
2026-09-08 13:27:29 -0300

    Agregue otro comentario

--- src/trabajopracticogruparjavagui/TrabajoPracticoGruparJavaGui.java
+++ src/trabajopracticogruparjavagui/TrabajoPracticoGruparJavaGui.java
@@ -16,6 +16,7 @@ public class TrabajoPracticoGruparJavaGui {
     public static void main(String[] args) {
         // TODO code application logic here
         //Hola, este es el primer comentario del proyecto
+        //Hola, este es el segundo comentario del proyecto
     }
     
 }
