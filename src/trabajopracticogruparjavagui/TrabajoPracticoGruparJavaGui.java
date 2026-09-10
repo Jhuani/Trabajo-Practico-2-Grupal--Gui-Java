a27e7ab5a09c46a682ac6a41e9d29df5ccbe32e8
“Juan <jil8989@gmail.com>
2026-09-10 12:40:56 -0300

    Update TrabajoPracticoGruparJavaGui.java
    probando conexion

--- src/trabajopracticogruparjavagui/TrabajoPracticoGruparJavaGui.java
+++ src/trabajopracticogruparjavagui/TrabajoPracticoGruparJavaGui.java
@@ -18,7 +18,7 @@ public class TrabajoPracticoGruparJavaGui {
     public static void main(String[] args) {
         // TODO code application logic here
         //Hola, este es el primer comentario del proyecto
-        //Hola, este es el segundo comentario del proyecto+
+        //Hola, este es el segundo comentario del proyecto+++
         try{
             for(javax.swing.UIManager.LookAndFeelInfo info
                     : javax.swing.UIManager.getInstalledLookAndFeels()){
